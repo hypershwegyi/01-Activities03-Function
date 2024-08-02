7de188ccb07546cda5cf1af31dc94c33958f1e62
hypershwegyi <shweyeetheint690@email.com>
2024-08-02 10:04:03 +0630

    third

---
+++ script.js
@@ -0,0 +1,13 @@
+var C = function(cat , dog) {
+    if(cat === dog) {
+        console.log("They are equal in type and value")
+    }
+
+    else if(cat == dog) {
+        console.log("They are equal in value")
+    }
+    else {
+        console.log("They values are not equal")
+    }
+}
+C(2,"2")
